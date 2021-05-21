8c5eef4f996a4f85ee1813dceabe5ff85b837141
Sanjit Dutta <sdlynx@gmail.com>
2021-05-20 22:37:59 -0400

    add another service

--- detect_noise_level.sh
+++ detect_noise_level.sh
@@ -3,6 +3,6 @@ while [ 1 ]
 do
 arecord -D plughw:1,0 -f cd -t wav -d 2 -r 16000 /home/pi/scripts/detect_noise/public/noise.wav
 volume=$(sox /home/pi/scripts/detect_noise/public/noise.wav -n stats -s 16 2>&1 | awk '/^Max\ level/ {print $3}')
-echo $volume > /home/pi/scripts/detect_noise/public/noise
+echo "<span>$volume</span>" > /home/pi/scripts/detect_noise/public/noise.html
 echo $volume
 done
